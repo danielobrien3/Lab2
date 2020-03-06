178d8ff98f52c769d3f4ee46056782931d5a2d81
Danny O'Brien <dannyobrien@Dannys-MacBook-Air.local>
2020-03-05 19:19:45 -0500

    Working proportional math

--- src/lab02/lab02.java
+++ src/lab02/lab02.java
@@ -23,18 +23,49 @@ public class lab02 {
 		
 		RegulatedMotor mLeft = new EV3LargeRegulatedMotor(MotorPort.A);
 		RegulatedMotor mRight = new EV3LargeRegulatedMotor(MotorPort.D);
-	
+		float k = 1000;
 		
 		SensorModes sideSensor = new NXTUltrasonicSensor(sidePort);
 		SensorModes frontSensor = new NXTUltrasonicSensor(frontPort);
 		
+		
 		SampleProvider sideSampler = sideSensor.getMode("Distance");
+		SampleProvider frontSampler = frontSensor.getMode("Distance");
 		float[] sideSamples = new float[sideSampler.sampleSize()];
+		float[] frontSamples = new float[frontSampler.sampleSize()];
+		mLeft.setSpeed(200);
 		while(true) {
+			mLeft.backward();
+			mRight.backward();
 			sideSampler.fetchSample(sideSamples, 0);
-			String str = Float.toString(sideSamples[0]);
-			//String str = Float.toString(sideSampler.fetchSample(0, offset););
+			frontSampler.fetchSample(frontSamples, 0);
+			float sideReading = sideSamples[0];
+			float frontReading = frontSamples[0];
+			float error = sideReading - 0.15f;
+			int rightSpeed = (int)(200 + k*error);
+			if(rightSpeed < 0 ) {
+				rightSpeed = 0;
+			}
+			mRight.setSpeed(rightSpeed);
+			/*if(error == 0) {
+				mRight.setSpeed(200);
+				mLeft.setSpeed(200);
+			}
+			if(error > 0) {
+				float rightSpeed = k*error;
+				mRight.setSpeed((int)rightSpeed);
+				mLeft.setSpeed(200);
+			}
+			if(error < 0) {
+				float leftSpeed = Math.abs(k*error);
+				mLeft.setSpeed((int)leftSpeed);
+				mRight.setSpeed(200);
+			}*/
+			String str = Float.toString(sideReading);
+			String str2 = Float.toString(frontSamples[0]);
+			lcddisplay.clear();
 			lcddisplay.drawString(str, 2, 4);
+			lcddisplay.drawInt(rightSpeed, 2, 6);
 		}
 	}
 }
